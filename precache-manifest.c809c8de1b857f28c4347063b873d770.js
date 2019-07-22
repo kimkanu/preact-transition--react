20de6c83c06ac118be8c3a0e50318dcb0da7bbbe
Keonwoo Kim <utophii@kaist.ac.kr>
2019-07-23 08:43:25 +0900

    Updates

---
+++ precache-manifest.c809c8de1b857f28c4347063b873d770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b496729dca3bfb6b4340aa38da4bc3a4",
+    "url": "/preact-transition--react/index.html"
+  },
+  {
+    "revision": "ef58fcd6378d9c6acbca",
+    "url": "/preact-transition--react/static/css/main.43fab277.chunk.css"
+  },
+  {
+    "revision": "f2ef91855c2b361cc8ef",
+    "url": "/preact-transition--react/static/js/2.64dfd442.chunk.js"
+  },
+  {
+    "revision": "ef58fcd6378d9c6acbca",
+    "url": "/preact-transition--react/static/js/main.52efb7a8.chunk.js"
+  },
+  {
+    "revision": "fb312d09b6f6f2df7afb",
+    "url": "/preact-transition--react/static/js/runtime~main.c19779c4.js"
+  }
+]);
